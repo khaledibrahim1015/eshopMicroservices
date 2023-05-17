3b0d622bc6bf2ab53eabbcfb18d8ea040f515d1a
KhaledIbrahim1015 <ibrahimkhaled136@gmail.com>
2023-05-17 10:03:05 +0300

    Define MongoDb configuration (Docker Container ) in AppSetting => Create Data Access Layer

---
+++ src/Services/Catalog/Catalog.API/Data/CatalogContext.cs
@@ -0,0 +1,30 @@
+﻿using Catalog.API.Entities;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+
+namespace Catalog.API.Data
+{
+    public class CatalogContext : ICatalogContext
+    {
+        public IMongoCollection<Product> Products { get; }
+
+        public CatalogContext(IConfiguration configuration)
+        {
+            // Setup connection with Mongodb (DockerContainer )
+            var client = new MongoClient(configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
+
+            // Get DataBase Name 
+            var db = client.GetDatabase(configuration.GetValue<string>("DatabaseSettings:DatabaseName"));
+
+            // Get Collection Name and assign it to prop
+
+            Products = db.GetCollection<Product>(configuration.GetValue<string>("DatabaseSettings:CollectionName"));
+
+
+
+        }
+
+
+
+    }
+}
