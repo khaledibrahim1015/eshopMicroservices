de2dedcdd75b20b3d250b992f2df93acf95647c0
KhaledIbrahim1015 <ibrahimkhaled136@gmail.com>
2023-05-17 10:53:04 +0300

    Create IProdcutRepository => ProdcutRepository Implementation

---
+++ src/Services/Catalog/Catalog.API/Repositories/ProductRepository.cs
@@ -0,0 +1,81 @@
+﻿using Catalog.API.Data;
+using Catalog.API.Entities;
+using MongoDB.Driver;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Catalog.API.Repositories
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly ICatalogContext _context;
+
+        public ProductRepository(ICatalogContext context)
+        {
+            _context = context;
+        }
+
+
+
+        public async Task<IEnumerable<Product>> GetProducts()
+        {
+            var filter = Builders<Product>.Filter.Empty;
+            return await _context.Products.Find(filter).ToListAsync();
+
+        }
+
+
+
+
+
+        public async Task<IEnumerable<Product>> GetProductsByCategory(string categoryName)
+        {
+            var filter = Builders<Product>.Filter.Eq(p => p.Category, categoryName);
+            return await _context.Products.Find(filter).ToListAsync();
+
+
+        }
+
+        public async Task<IEnumerable<Product>> GetProductsByName(string productName)
+        {
+            var filter = new FilterDefinitionBuilder<Product>().Eq(p => p.Name, productName);
+            return await _context.Products.Find(filter).ToListAsync();
+
+
+        }
+
+        public async Task<Product> GetProductById(string id)
+        {
+
+            return await _context.Products.Find(p => p.Id == id).FirstOrDefaultAsync();
+
+        }
+
+        public async Task CreateProduct(Product product)
+        {
+           await _context.Products.InsertOneAsync(product);
+        }
+
+       
+
+        public async Task<bool> UpdateProduct(Product product)
+        {
+           
+            var filter  =  Builders<Product>.Filter.Eq(p => p.Id ,product.Id);
+
+           var updateResult= await _context.Products.ReplaceOneAsync(filter: filter, replacement: product);
+
+            return updateResult.IsAcknowledged && updateResult.ModifiedCount > 0;
+
+        }
+
+
+
+        public Task<bool> DeleteProduct(string id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+
+    }
+}
